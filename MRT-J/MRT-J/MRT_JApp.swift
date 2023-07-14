6a8115fd1fea1fb6eb9696041bb83625d64f5b4a
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-07-14 16:01:30 +0700

    First Init

---
+++ MRT-J/MRT-J/MRT_JApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRT_JApp.swift
+//  MRT-J
+//
+//  Created by Leonardo Wijaya on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MRT_JApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
